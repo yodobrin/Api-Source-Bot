5c2528f348b0ea8dc2696d65b56ee89a10b72151
Yoav Dobrin <yodobrin@microsoft.com>
2018-07-08 18:03:27 +0300

    initial version

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=apisourcebot-Web-Deploy.pubxml -p:Password=oKcwq9ZqGWpq1lEPF8dyxrReKQytblamYm04TuDAvsyWLoGsLAPFQZddbpol
+
